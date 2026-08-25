3004793a62a97a87cf7f6704f490e284ee547a59
Pankaj Baria <pankajbaria712@gmail.com>
2026-08-25 17:44:55 +0530

    Login & Registration page UI done ✅

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { RouterProvider } from "react-router";
+import { router } from "./app.routes.jsx";
 
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+  
